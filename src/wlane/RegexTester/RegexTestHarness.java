c06bf9e58823f4c6ddc1f439f63b25abb4ad7edc
wlane <will@asklane.com>
2015-04-11 15:23:16 -0600

    Initial commit

---
+++ src/wlane/RegexTester/RegexTestHarness.java
@@ -0,0 +1,36 @@
+package wlane.RegexTester;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.*;
+
+public class RegexTestHarness {
+
+    public static void main(String[] args) throws IOException {
+
+        //Initialized user input reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.println("Enter your regex: ");
+            Pattern pattern =
+                    Pattern.compile(br.readLine());
+
+            System.out.println("Enter input string to search: ");
+            Matcher matcher =
+                    pattern.matcher(br.readLine());
+
+            boolean found = false;
+            while (matcher.find()) {
+                System.out.println("Text Matched: " + matcher.group() +" starting at index: "
+                        + matcher.start() + " and ending at index: " + matcher.end());
+                found = true;
+            }
+            if(!found){
+                System.out.println("No match found.%n");
+            }
+        }
+    }
+
+}
